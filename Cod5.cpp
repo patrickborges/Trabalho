e0a0043ae087bbb13bed54d1a04be5e2f5705bbc
patrickborges <patrickborges@users.noreply.github.com>
2014-08-20 20:31:01 -0300

    Create Cod5.cpp

---
+++ Cod5.cpp
@@ -0,0 +1,16 @@
+// getche()
+
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+
+{
+    char ch;
+    printf ("Digite um caracter: ");
+    ch = getche();
+    printf ("\nA tecla pressionada foi: %c \n", ch);
+
+    return 0;
+
+}
