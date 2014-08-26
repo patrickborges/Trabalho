e717d7d63289cf13d8b8a69c6bc27b7f27c8d4be
patrickborges <patrickborges@users.noreply.github.com>
2014-08-25 22:07:37 -0300

    Create Cod07_While.cpp

---
+++ Cod07_While.cpp
@@ -0,0 +1,18 @@
+// using While
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n = 10;
+
+    while (n>0) {
+        cout << n << ",";
+        n--;
+    }
+
+    cout << "liftoff" << endl;
+    return 0;
+}
